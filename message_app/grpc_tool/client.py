760ddb9a876c69bdb84fad3d4488f44c18e605d1
cityking <154443603@qq.com>
2019-07-22 14:58:03 +0800

    添加短信记录

--- message_app/grpc_tool/client.py
+++ message_app/grpc_tool/client.py
@@ -2,11 +2,11 @@ import grpc
 import json
 import charge_pb2 as data_pb2, charge_pb2_grpc as data_pb2_grpc
 
-#_HOST = 'localhost'
-#_PORT = '8800'
+_HOST = 'localhost'
+_PORT = '8803'
 
-_HOST = '120.77.237.231'
-_PORT = '9274'
+#_HOST = '120.77.237.231'
+#_PORT = '9274'
 conn = grpc.insecure_channel(_HOST + ':' + _PORT)
 
 def get_chargeinfo(business_id):
@@ -65,11 +65,28 @@ def query_orderlist(business_id):
     data = json.loads(response.text) 
     return data 
 
+def add_send_record(business_id, phone, content):
+    client = data_pb2_grpc.MessageChargeStub(channel=conn)
+    data = dict(business_id=business_id,
+            phone=phone,
+            content=content)
+    print(data)
+    text = json.dumps(data)
+    import pdb;pdb.set_trace()
+    response = client.AddMsgSendRecord.future(data_pb2.json(text=text))
+    response = response.result()
+    data = json.loads(response.text) 
+    return data 
+
+
+
 
 if __name__ == '__main__':
 #    data = get_chargeinfo(1)
 #    data = check_order(1, '15633887877173738')
-    data = unified_order(1, 100, 0.01, 2)
+#    data = unified_order(1, 100, 0.01, 2)
 #    data = query_businessinfo(1)
 #    data = query_orderlist(1)
+    data = add_send_record(1, '18233228976', 'skjjksjkjkk')
+    
     print(data)
